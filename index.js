4bacbed3d580f384aecb82332fd5b680bd5a70b6
James Overbay <orangetoque13@gmail.com>
2018-01-07 05:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ var lowercase = "hello"
 function sayHiToGrandma(string) {
   if (string === "hello")
   return "I can't hear you!"
-} else if (string === "HELLO")
+  else if (string === "HELLO")
+  return ("YES INDEED!")
+}
